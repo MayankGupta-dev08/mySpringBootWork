ed800be2c2aa9dbdca12bfe2ec3e096a1ba338e1
Mayank Gupta <ce.mayank8@gmail.com>
2023-11-01 14:44:34 +0530

    starting spring core

---
+++ learn-spring-core/src/main/java/com/devmayankg/learnspringcore/LearnSpringCoreApplication.java
@@ -0,0 +1,13 @@
+package com.devmayankg.learnspringcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringCoreApplication.class, args);
+	}
+
+}
---
+++ learn-spring-core/src/test/java/com/devmayankg/learnspringcore/LearnSpringCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmayankg.learnspringcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
