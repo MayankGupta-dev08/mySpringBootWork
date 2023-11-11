ae8c59e8e8d8b08ad70759548b6f7085b4d0bee8
Mayank Gupta <ce.mayank8@gmail.com>
2023-11-11 19:23:11 +0530

    spring core: Important Note about Prototype beans

--- learn-spring-core/src/main/java/com/devmayankg/learnspringcore/util/CricketCoach.java
+++ learn-spring-core/src/main/java/com/devmayankg/learnspringcore/util/CricketCoach.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+/**Prototype beans are lazy by default. There is no need to use the @Lazy annotation for prototype scopes beans.*/
 @Component
 //@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)   //by default --> only single instance for a bean
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
@@ -24,6 +25,7 @@ public class CricketCoach implements Coach {
     //pre destruction of bean
     /**
      * For "prototype" scoped beans, Spring does not call the destroy method. Gasp!
+     * The client code must clean up prototype-scoped objects and release expensive resources that the prototype bean(s) are holding.
      * */
     @PreDestroy
     private void cleanUp() {
