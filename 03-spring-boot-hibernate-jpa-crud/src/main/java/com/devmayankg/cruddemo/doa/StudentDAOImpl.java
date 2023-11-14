c51d40b24dedfaf62e619e6c7dda4bdf592baf9d
Mayank Gupta <ce.mayank8@gmail.com>
2023-11-14 18:31:28 +0530

    hibernate-jpa-crud: implemented update methods

--- 03-spring-boot-hibernate-jpa-crud/src/main/java/com/devmayankg/cruddemo/doa/StudentDAOImpl.java
+++ 03-spring-boot-hibernate-jpa-crud/src/main/java/com/devmayankg/cruddemo/doa/StudentDAOImpl.java
@@ -12,7 +12,6 @@ import java.util.List;
 /**
  * All JPQL (Jakarta Persistence API Query Language) is based on Entity Name and Entity fields.
  */
-
 @Repository
 public class StudentDAOImpl implements DataAccessObjectI<Student> {
 
@@ -27,62 +26,109 @@ public class StudentDAOImpl implements DataAccessObjectI<Student> {
     // POST
     @Override
     @Transactional
-    public void postData(Student student) {
+    public void postEntity(Student student) {
         entityManager.persist(student);
         System.out.println("Saved the detail of: " + student.toString());
     }
 
     // GET
+    /**
+     * Since Id is a Primary Key we can use find() and it will always return result/null.
+     */
     @Override
-    public Student getDataById(int id) {
+    public Student getEntityById(int id) {
         System.out.println("Finding... the details for the id: " + id);
         return entityManager.find(Student.class, id);
     }
 
     // GET
     @Override
-    public List<Student> getAllData() {
-        System.out.println("Fetching entries for all the students....");
+    public List<Student> getAllEntities() {
+        System.out.println("Fetching all the entries from the Student Table....");
         return executeQuery("", "");
     }
 
     // GET
     @Override
-    public List<Student> getDataByFirstName(String firstName) {
+    public List<Student> getEntitiesByFirstName(String firstName) {
         System.out.println("Fetching entries for all the students whose firstName=" + firstName);
         // NOTE: for the query entity field names will be exact as the fieldName used in class (firstName and not first_name)
         return executeQuery(" WHERE firstName='" + firstName + "'", " ORDER BY firstName asc");
     }
 
     // GET
-    @Override
-    public List<Student> getDataByLastName(String lastName) {
-        System.out.println("Fetching entries for all the students whose lastName=" + lastName);
-        // NOTE: for the query entity field names will be exact as the fieldName used in class (lastName and not last_name)
-        return executeQuery(" WHERE lastName='" + lastName + "'", " ORDER BY lastName desc");
-    }
-
-    // GET
-    public List<Student> getDataByEmail(String email) {
+    public List<Student> getEntitiesByEmail(String email) {
         System.out.println("Fetching entries for all the students whose email is LIKE '" + email + "'");
         return executeQuery(" WHERE email LIKE '" + email + "'", " ORDER BY id desc");
     }
 
     // PUT
     @Override
-    public boolean updateById(int id) {
+    @Transactional
+    public boolean updateFirstNameById(int id, String firstName) {
+        Student student = getEntityById(id);
+        if (student != null) {
+            student.setFirstName(firstName);
+            entityManager.merge(student);
+            System.out.println("Updated student with " + id + ": " + student.toString());
+            return true;
+        }
+        System.out.println("Could not find a student with id: " + id);
         return false;
     }
 
+    // PUT
+    @Override
+    @Transactional
+    public boolean updateFieldOfEntityById(int id, String field, String value) {
+        if (List.of("firstName", "lastName", "email").stream()
+                .anyMatch(f -> f.toLowerCase().equalsIgnoreCase(field))) {
+            Student student = getEntityById(id);
+            if (student != null) {
+                switch (field.toLowerCase()) {
+                    case "firstname" -> student.setFirstName(value);
+                    case "lastname" -> student.setLastName(value);
+                    case "email" -> student.setEmail(value);
+                    default -> System.out.println("Invalid field entered!!");
+                }
+                entityManager.merge(student);
+                System.out.println("Updated student with " + id + ": " + student.toString());
+                return true;
+            } else {
+                System.out.println("Could not find a student with id: " + id);
+                return false;
+            }
+        }
+        System.out.println("Error: Incorrect field entered for update method");
+        return false;
+    }
+
+    // PUT
+    @Override
+    @Transactional
+    public int updateByQuery(String field, String value) {
+        if (List.of("firstName", "lastName", "email").stream()
+                .anyMatch(f -> f.toLowerCase().equalsIgnoreCase(field))) {
+            Class<Student> myClass = Student.class;
+            String jpaEntity = myClass.getSimpleName();
+            String ql = "UPDATE " + jpaEntity + " SET " + field + "='" + value + "'";
+            int rowsUpdated = entityManager.createQuery(ql, myClass).executeUpdate();
+            System.out.println("Success: Updated rows " + rowsUpdated);
+            return rowsUpdated;
+        }
+        System.out.println("Error: Incorrect field entered for update method");
+        return -1;
+    }
+
     // DELETE
     @Override
-    public void deleteById() {
+    public void deleteEntityById() {
 
     }
 
     // DELETE
     @Override
-    public void deleteAll() {
+    public void deleteAllEntities() {
 
     }
 
