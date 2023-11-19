4b0f84c3505e9f91a34c373495059bce3e10990e
Mayank Gupta <ce.mayank8@gmail.com>
2023-11-19 16:50:35 +0530

    spring-boot-emp_rest_db_app: refactored RestWebAppApplication

--- 05-spring-boot-my-rest-service/src/main/java/com/dev/mayang/restwebapp/RestWebAppApplication.java
+++ 05-spring-boot-my-rest-service/src/main/java/com/dev/mayang/restwebapp/RestWebAppApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RestWebAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestWebAppApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(RestWebAppApplication.class, args);
+    }
 }
