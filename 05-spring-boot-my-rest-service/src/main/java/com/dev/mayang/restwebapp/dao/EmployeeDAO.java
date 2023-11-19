293ea34a37a58180000a8a2c08a407a442512a3b
Mayank Gupta <ce.mayank8@gmail.com>
2023-11-19 13:58:27 +0530

    spring-boot-emp_rest_db_app: added basic EmployeeDAO class

--- 05-spring-boot-my-rest-service/src/main/java/com/dev/mayang/restwebapp/dao/EmployeeDAO.java
+++ 05-spring-boot-my-rest-service/src/main/java/com/dev/mayang/restwebapp/dao/EmployeeDAO.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class EmployeeDAO implements DataAccessObject<Employee> {
     @Override
     public void postEntity(Employee obj) {
-        
+
     }
 
     @Override
