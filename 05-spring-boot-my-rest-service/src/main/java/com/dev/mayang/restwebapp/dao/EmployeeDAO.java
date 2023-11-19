ecf824eff3967ee9a4f81bd48b81011d38d7b6d7
Mayank Gupta <ce.mayank8@gmail.com>
2023-11-19 13:51:21 +0530

    spring-boot-emp_rest_db_app: added basic EmployeeDAO class

---
+++ 05-spring-boot-my-rest-service/src/main/java/com/dev/mayang/restwebapp/dao/EmployeeDAO.java
@@ -0,0 +1,52 @@
+package com.dev.mayang.restwebapp.dao;
+
+import com.dev.mayang.restwebapp.entity.Employee;
+
+import java.util.List;
+
+public class EmployeeDAO implements DataAccessObject<Employee> {
+    @Override
+    public void postEntity(Employee obj) {
+        
+    }
+
+    @Override
+    public Employee getEntityById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Employee> getAllEntities() {
+        return null;
+    }
+
+    @Override
+    public List<Employee> getEntitiesByQueryingField(String field, String value, boolean isLike, String orderByField, boolean isAsc) {
+        return null;
+    }
+
+    @Override
+    public boolean updateFieldOfEntityById(int id, String field, String value) {
+        return false;
+    }
+
+    @Override
+    public int updateAllEntitiesByQuery(String field, String value) {
+        return 0;
+    }
+
+    @Override
+    public boolean deleteEntityById(int id) {
+        return false;
+    }
+
+    @Override
+    public int deleteEntityByQuery(String field, String value, boolean isLikeOperator) {
+        return 0;
+    }
+
+    @Override
+    public int deleteAllEntities() {
+        return 0;
+    }
+}
