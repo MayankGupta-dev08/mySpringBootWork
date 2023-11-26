0d0c74da92d8144b901b35d6d4a2aa617a6b2a02
Mayank Gupta <ce.mayank8@gmail.com>
2023-11-26 18:26:21 +0530

    spring-mvc-thymeleaf: basic app

---
+++ 10-spring-boot-mvc-thymeleaf/src/test/java/dev/mayang/thymeleaf/demo/ThymeleafDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mayang.thymeleaf.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
