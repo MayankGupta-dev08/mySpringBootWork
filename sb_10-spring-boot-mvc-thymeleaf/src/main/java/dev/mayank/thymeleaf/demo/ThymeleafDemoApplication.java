51ec345f56ded80074ec0cc867d85e3a6b418bae
Mayank Gupta <ce.mayank8@gmail.com>
2024-04-21 18:58:41 +0530

    setting up modules for futher work...

---
+++ sb_10-spring-boot-mvc-thymeleaf/src/main/java/dev/mayank/thymeleaf/demo/ThymeleafDemoApplication.java
@@ -0,0 +1,12 @@
+package dev.mayank.thymeleaf.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafDemoApplication.class, args);
+    }
+}
---
+++ sb_11-spring-boot-some-work/src/main/java/dev/mayank/App.java
@@ -0,0 +1,7 @@
+package dev.mayank;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
