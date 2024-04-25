6fa9faccf461718bef77e43053f21a7f723286ef
Mayank Gupta <ce.mayank8@gmail.com>
2024-04-26 00:34:38 +0530

    sb_11-spring-boot-mvc-thymefeaf: RESTful Service with Spring Boot, Spring MVC and Thymefeaf

--- sb_11-spring-boot-mvc-thymeleaf/src/main/java/dev/mayank/thymeleaf/demo/controller/exc2/Controller_Exercise2.java
+++ sb_11-spring-boot-mvc-thymeleaf/src/main/java/dev/mayank/thymeleaf/demo/controller/exc2/Controller_Exercise2.java
@@ -1,8 +1,11 @@
 package dev.mayank.thymeleaf.demo.controller.exc2;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Exercise2: Reading Form Data using Spring MVC
@@ -28,5 +31,28 @@ public class Controller_Exercise2 {
     @GetMapping("/processForm")
     public String processForm() {
         return "processForm_ex2";
+    }
+
+    /**
+     * controller method @<a href="http://localhost:8080/exc2/processFormV2">processFormV2</a>
+     * to process the HTML form.
+     */
+    @GetMapping("/processFormV2")
+    public String processFormV2(HttpServletRequest httpServletRequest, Model model) {
+        String studentName = httpServletRequest.getParameter("studentName");
+        String msg = "Hey there, Welcome %s.".formatted(studentName.toUpperCase());
+        model.addAttribute("message", msg);
+        return "processFormV2_ex2";
+    }
+
+    /**
+     * controller method @<a href="http://localhost:8080/exc2/processFormV3">processFormV3</a>
+     * to process the HTML form.
+     */
+    @GetMapping("/processFormV3")
+    public String processFormV3(@RequestParam("studentName") String theStudentName, Model model) {
+        String msg = "Hola, bienvenido %s.".formatted(theStudentName.toUpperCase());
+        model.addAttribute("message", msg);
+        return "processFormV2_ex2";
     }
 }
