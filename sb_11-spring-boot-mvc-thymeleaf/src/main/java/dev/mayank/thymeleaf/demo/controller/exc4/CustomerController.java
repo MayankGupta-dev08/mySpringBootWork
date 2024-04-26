338a6145cf87eb4dca22ce2affa04b547fd7e446
Mayank Gupta <ce.mayank8@gmail.com>
2024-04-27 01:33:27 +0530

    sb_11-spring-boot-mvc-thymefeaf: RESTful Service with Spring Boot, Spring MVC and Thymefeaf

--- sb_11-spring-boot-mvc-thymeleaf/src/main/java/dev/mayank/thymeleaf/demo/controller/exc4/CustomerController.java
+++ sb_11-spring-boot-mvc-thymeleaf/src/main/java/dev/mayank/thymeleaf/demo/controller/exc4/CustomerController.java
@@ -1,12 +1,13 @@
 package dev.mayank.thymeleaf.demo.controller.exc4;
 
 import dev.mayank.thymeleaf.demo.model.Customer;
+import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Exercise4: Form Validation and Required Fields using Spring MVC
@@ -17,21 +18,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class CustomerController {
 
     /**
-     * controller method @<a href="http://localhost:8080/customers/showCustomerForm">showCustomerForm</a>
+     * controller method <a href="http://localhost:8080/customers/showCustomerForm">showCustomerForm</a>
      * to show the initial HTML form.
      */
     @GetMapping("/showCustomerForm")
     public String showForm(Model model) {
+        model.addAttribute("customer", new Customer());
         return "customerSignupForm";
     }
 
     /**
-     * controller method @<a href="http://localhost:8080/customers/processCustomerForm">processCustomerForm</a>
+     * controller method <a href="http://localhost:8080/customers/processCustomerForm">processCustomerForm</a>
      * to process the HTML form.
+     *
+     * @@Valid --> Tells the Spring MVC to perform validation
+     * @BindingResult --> store the results of the validation
      */
     @PostMapping("/processCustomerForm")
-    public String processForm(@ModelAttribute("customer") Customer customer) {
+    public String processForm(@Valid @ModelAttribute("customer") Customer customer,
+                              BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "customerSignupForm";
+
         System.out.println(customer);    //logging the data
         return "customerConfirmationForm";
+    }
+
+    /**
+     * To preprocess the web request at controller level to trim the leading and trailing white spaces.
+     */
+    @InitBinder
+    private void preprocessWebRequest(WebDataBinder webDataBinder) {
+        webDataBinder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
     }
 }
--- sb_11-spring-boot-mvc-thymeleaf/src/main/java/dev/mayank/thymeleaf/demo/model/Customer.java
+++ sb_11-spring-boot-mvc-thymeleaf/src/main/java/dev/mayank/thymeleaf/demo/model/Customer.java
@@ -1,5 +1,9 @@
 package dev.mayank.thymeleaf.demo.model;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,10 +13,20 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public final class Customer {
 
+    @NotNull(message = "required")
+    @Size(min = 1, max = 10, message = "min:1, max:10")
     private String firstName;
+
+    @Size(max = 10, message = "max:10")
     private String lastName;
+
+    @Pattern(regexp = "^[A-Z]{3}[0-9]{2}", message = "first 3 characters to be alphabets, rest 2 should be numeric")
     private String postalCode;
+
+    @Min(value = 1, message = "at least 1 is required")
+    @Min(value = 5, message = "at max 5 is possible")
     private int freePasses;
+
     private String courseCode;
 
 }
