cf0114ee51ff6e7927f25d41c3abef5108ca0385
Mayank Gupta <ce.mayank8@gmail.com>
2024-05-02 01:56:16 +0530

    sb_10-spring-boot-rest-service-spring-security-jpa: RESTful Service with Spring Security and JPA/Hibernate

--- sb_10-spring-rest-service-security-jpa/src/main/resources/sql-scripts/01-employee-directory.sql
+++ sb_10-spring-rest-service-security-jpa/src/main/resources/sql-scripts/01-employee-directory.sql
@@ -1,12 +1,17 @@
+--
+-- Creating the `employee_directory` database if not existing and using it.
+--
 CREATE DATABASE  IF NOT EXISTS `employee_directory`;
 USE `employee_directory`;
 
 --
--- Table structure for table `employee`
+-- Dropping the table `employee` if already existing
 --
-
 DROP TABLE IF EXISTS `employee`;
 
+--
+-- Table structure for table `employee`
+--
 CREATE TABLE `employee` (
   `id` int NOT NULL AUTO_INCREMENT,
   `first_name` varchar(45) DEFAULT NULL,
@@ -18,7 +23,6 @@ CREATE TABLE `employee` (
 --
 -- Data for table `employee`
 --
-
 INSERT INTO `employee` VALUES 
 	(1,'Leslie','Andrews','leslie@luv2code.com'),
 	(2,'Emma','Baumgarten','emma@luv2code.com'),
@@ -26,3 +30,7 @@ INSERT INTO `employee` VALUES
 	(4,'Yuri','Petrov','yuri@luv2code.com'),
 	(5,'Juan','Vega','juan@luv2code.com');
 
+---
+-- SHOWING ALL ENTRIES FOR THE 'employee' TABLE
+---
+SELECT * FROM employee_directory.employee;
