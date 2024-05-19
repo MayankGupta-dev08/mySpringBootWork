3fb806a5ebf33d9c56edfdcb819edb9af67f90e2
Mayank Gupta <ce.mayank8@gmail.com>
2024-05-19 18:12:21 +0530

    sb_14-spring-boot-jpa-hibernate-adv: Instructor CLI App with Spring Boot, Spring Data JPA & Hibernate OneToOne Uni/Bi-directional Mapping

--- sb_14-spring-boot-jpa-hibernate-1to1-bi/src/main/java/dev/mayank/cli/app/dao/InstructorDAOImpl.java
+++ sb_14-spring-boot-jpa-hibernate-1to1-bi/src/main/java/dev/mayank/cli/app/dao/InstructorDAOImpl.java
@@ -4,14 +4,19 @@ import dev.mayank.cli.app.entity.Instructor;
 import dev.mayank.cli.app.entity.InstructorDetail;
 import jakarta.persistence.EntityManager;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository
-@AllArgsConstructor
 @SuppressWarnings("unused")
 public class InstructorDAOImpl implements InstructorDAO {
     private EntityManager entityManager;
+
+    @Autowired
+    public InstructorDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
 
     @Override
     @Transactional
