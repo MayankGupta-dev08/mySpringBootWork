79f63b5dfbedbf604fe2fb10cdda79762a69a666
Mayank Gupta <ce.mayank8@gmail.com>
2024-08-11 16:49:44 +0530

    infinity-schoolhouse app: adapting thymeleaf for dynamic content (Spring MVC)

--- sb_16-project-infinity-schoolhouse/src/main/java/dev/mayank/infinityschoolhouse/controller/RestController.java
+++ sb_16-project-infinity-schoolhouse/src/main/java/dev/mayank/infinityschoolhouse/controller/RestController.java
@@ -1,13 +1,15 @@
 package dev.mayank.infinityschoolhouse.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RestController {
 
-    @RequestMapping("/home")
-    public String displayHomePage() {
+    @RequestMapping(value = {"", "/", "/home"})
+    public String displayHomePage(Model model) {
+        model.addAttribute("username", "Learner");
         return "home.html";
     }
 }
