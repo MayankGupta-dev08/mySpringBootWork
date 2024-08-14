8483a4c55f0631cfcae771bbe87b13653eb4f131
Mayank Gupta <ce.mayank8@gmail.com>
2024-08-15 00:07:43 +0530

    infinity-schoolhouse app: added both frontend and server-side validations on contact form

--- sb_16-project-infinity-schoolhouse/src/main/java/dev/mayank/infinityschoolhouse/model/ContactDetail.java
+++ sb_16-project-infinity-schoolhouse/src/main/java/dev/mayank/infinityschoolhouse/model/ContactDetail.java
@@ -1,13 +1,34 @@
 package dev.mayank.infinityschoolhouse.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 import java.text.MessageFormat;
 
 public class ContactDetail {
+    @NotBlank(message="Name must not be blank")
+    @Size(min=3, message="Name must be at least 3 characters long")
     private String name;
+
+    @NotBlank(message="Mobile number must not be blank")
+    @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
     private String mobileNum;
+
+    @NotBlank(message="Email must not be blank")
+    @Email(message = "Please provide a valid email address" )
     private String email;
+
+    @NotBlank(message="Subject must not be blank")
+    @Size(min=5, message="Subject must be at least 5 characters long")
     private String subject;
+
+    @NotBlank(message="Message must not be blank")
+    @Size(min=10, message="Message must be at least 10 characters long")
     private String message;
+
+    public ContactDetail() {}
 
     public ContactDetail(String name, String mobileNum, String email, String subject, String message) {
         this.name = name;
