b57bb32198c40b767afd274a9667d362aa009bbe
Mayank Gupta <ce.mayank8@gmail.com>
2024-08-21 23:50:32 +0530

    infinity-schoolhouse app: added h2 db

--- sb_16-project-infinity-schoolhouse/src/main/java/dev/mayank/infinityschoolhouse/config/SecurityConfig.java
+++ sb_16-project-infinity-schoolhouse/src/main/java/dev/mayank/infinityschoolhouse/config/SecurityConfig.java
@@ -7,6 +7,9 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -15,20 +18,21 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class SecurityConfig {
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, HandlerMappingIntrospector introspector) throws Exception {
         httpSecurity.csrf(csrf -> csrf.ignoringRequestMatchers("/saveMsg"));
         httpSecurity.authorizeRequests(requests -> {
             requests
-                    .requestMatchers("/about").permitAll()
-                    .requestMatchers("/assets/**").permitAll()
-                    .requestMatchers("/contact").permitAll()
-                    .requestMatchers("/courses").permitAll()
-                    .requestMatchers("/dashboard").authenticated()
-                    .requestMatchers("/holidays/**").permitAll()
-                    .requestMatchers("/", "/home").permitAll()
-                    .requestMatchers("/login").permitAll()
-                    .requestMatchers("/logout").permitAll()
-                    .requestMatchers("/saveMsg").permitAll();
+                    .requestMatchers(new MvcRequestMatcher(introspector, "/about")).permitAll()
+                    .requestMatchers(new MvcRequestMatcher(introspector, "/assets/)**")).permitAll()
+                    .requestMatchers(new MvcRequestMatcher(introspector, "/contact")).permitAll()
+                    .requestMatchers(new MvcRequestMatcher(introspector, "/courses")).permitAll()
+                    .requestMatchers(new MvcRequestMatcher(introspector, "/dashboard")).authenticated()
+                    .requestMatchers(new MvcRequestMatcher(introspector, "/holidays/)**")).permitAll()
+                    .requestMatchers(new MvcRequestMatcher(introspector, "/home")).permitAll()
+                    .requestMatchers(new MvcRequestMatcher(introspector, "/login")).permitAll()
+                    .requestMatchers(new MvcRequestMatcher(introspector, "/logout")).permitAll()
+                    .requestMatchers(new MvcRequestMatcher(introspector, "/saveMsg")).permitAll()
+                    .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll();
         });
         httpSecurity.formLogin(formLogin -> {
             formLogin
