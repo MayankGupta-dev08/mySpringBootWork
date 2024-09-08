8ecb119723a101976c46f07c0e99bef7c4c953c5
Mayank Gupta <ce.mayank8@gmail.com>
2024-09-08 15:39:49 +0530

    ish app: added custom authentication provider using person & role

--- sb_16-project-infinity-schoolhouse/src/main/java/dev/mayank/infinityschoolhouse/config/SecurityConfig.java
+++ sb_16-project-infinity-schoolhouse/src/main/java/dev/mayank/infinityschoolhouse/config/SecurityConfig.java
@@ -4,9 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
@@ -44,22 +41,5 @@ public class SecurityConfig {
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
-    }
-
-    @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("12345")
-                .roles("USER")
-                .build();
-
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("admin")
-                .password("54321")
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(user, admin);
     }
 }
---
+++ sb_16-project-infinity-schoolhouse/src/main/java/dev/mayank/infinityschoolhouse/security/ISHUserAuthenticationProvider.java
@@ -0,0 +1,51 @@
+package dev.mayank.infinityschoolhouse.security;
+
+import dev.mayank.infinityschoolhouse.model.Person;
+import dev.mayank.infinityschoolhouse.model.Role;
+import dev.mayank.infinityschoolhouse.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Component
+@SuppressWarnings("unused")
+public class ISHUserAuthenticationProvider implements AuthenticationProvider {
+    private static final String ROLE_PREFIX = "ROLE_";
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String emailAddress = authentication.getName();
+        String password = authentication.getCredentials().toString();
+        Person person = personRepository.readByEmail(emailAddress);
+        if (null != person && person.getPersonId() > 0 && password.equals(person.getPwd())) {
+            return new UsernamePasswordAuthenticationToken(person.getName(), password,
+                    getGrantedAuthorities(person.getRole()));
+        }
+
+        throw new BadCredentialsException("Invalid credentials!");
+    }
+
+    private Collection<? extends GrantedAuthority> getGrantedAuthorities(Role role) {
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        grantedAuthorities.add(new SimpleGrantedAuthority(ROLE_PREFIX + role.getRoleName()));
+        return grantedAuthorities;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
