64a719a28e914ccd615bb3110bcf6b76a688d5fd
Mayank Gupta <ce.mayank8@gmail.com>
2024-09-20 10:10:16 +0530

    ish app: fixed some bugs in ish proxy service app

--- sb_17-ish-proxy-service/src/main/java/dev/mayank/ishproxyservice/config/AppConfig.java
+++ sb_17-ish-proxy-service/src/main/java/dev/mayank/ishproxyservice/config/AppConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@SuppressWarnings("unused")
 public class AppConfig {
 
     @Bean
