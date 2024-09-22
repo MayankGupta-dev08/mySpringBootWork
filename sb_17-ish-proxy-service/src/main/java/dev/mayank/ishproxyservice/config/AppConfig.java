0a4ac2f7d97f126d79832229b04281a0deaab628
Mayank Gupta <ce.mayank8@gmail.com>
2024-09-22 22:38:11 +0530

    ish app: consuming rest service using web client

--- sb_17-ish-proxy-service/src/main/java/dev/mayank/ishproxyservice/config/AppConfig.java
+++ sb_17-ish-proxy-service/src/main/java/dev/mayank/ishproxyservice/config/AppConfig.java
@@ -5,8 +5,11 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunctions;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
+@SuppressWarnings("unused")
 public class AppConfig {
 
     @Bean
@@ -18,5 +21,14 @@ public class AppConfig {
     public RestTemplate restTemplate() {
         RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder();
         return restTemplateBuilder.basicAuthentication("ts_admin@ish.com", "admin").build();
+    }
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder()
+                .filter(
+                        ExchangeFilterFunctions.basicAuthentication("ts_admin@ish.com", "admin")
+                )
+                .build();
     }
 }
--- sb_17-ish-proxy-service/src/main/java/dev/mayank/ishproxyservice/controller/ContactProxyRestController.java
+++ sb_17-ish-proxy-service/src/main/java/dev/mayank/ishproxyservice/controller/ContactProxyRestController.java
@@ -11,6 +11,8 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -20,11 +22,13 @@ import java.util.List;
 public class ContactProxyRestController {
     private final ContactMsgProxy contactMsgProxy;
     private final RestTemplate restTemplate;
+    private final WebClient webClient;
 
     @Autowired
-    public ContactProxyRestController(ContactMsgProxy contactMsgProxy, RestTemplate restTemplate) {
+    public ContactProxyRestController(ContactMsgProxy contactMsgProxy, RestTemplate restTemplate, WebClient webClient) {
         this.contactMsgProxy = contactMsgProxy;
         this.restTemplate = restTemplate;
+        this.webClient = webClient;
     }
 
     @GetMapping("/getMessages")
@@ -35,7 +39,7 @@ public class ContactProxyRestController {
 
     @PostMapping("/sendMessage")
     public ResponseEntity<Response> sendMessage(@RequestBody ContactDetail message) {
-        log.info("Sending message to contact-msg-service");
+        log.info("Sending message using restTemplate");
         String uri = "http://localhost:8080/api/contact/saveMessage";
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.add("Content-Type", "application/json");
@@ -44,5 +48,17 @@ public class ContactProxyRestController {
         ResponseEntity<Response> responseEntity = restTemplate.exchange(
                 uri, HttpMethod.POST, httpEntity, Response.class);
         return responseEntity;
+    }
+
+    @PostMapping("/saveMsg")
+    public Mono<Response> saveMsg(@RequestBody ContactDetail message) {
+        log.info("Saving message using webClient");
+        String uri = "http://localhost:8080/api/contact/saveMessage";
+        return webClient.post().uri(uri)
+                .header("Content-Type", "application/json")
+                .header("invocationFrom", "webClient")
+                .body(Mono.just(message), ContactDetail.class)
+                .retrieve()
+                .bodyToMono(Response.class);
     }
 }
