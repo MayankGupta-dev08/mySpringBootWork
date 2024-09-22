3bec2e1a62e703435506949870ad52bfed77fe7f
Mayank Gupta <ce.mayank8@gmail.com>
2024-09-22 21:45:51 +0530

    ish app: added saveMsg endpoint using restTemplate for proxy service

--- sb_17-ish-proxy-service/src/main/java/dev/mayank/ishproxyservice/config/AppConfig.java
+++ sb_17-ish-proxy-service/src/main/java/dev/mayank/ishproxyservice/config/AppConfig.java
@@ -1,8 +1,10 @@
 package dev.mayank.ishproxyservice.config;
 
 import feign.auth.BasicAuthRequestInterceptor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
@@ -10,5 +12,11 @@ public class AppConfig {
     @Bean
     public BasicAuthRequestInterceptor basicAuthRequestInterceptor() {
         return new BasicAuthRequestInterceptor("ts_admin@ish.com", "admin");
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder();
+        return restTemplateBuilder.basicAuthentication("ts_admin@ish.com", "admin").build();
     }
 }
--- sb_17-ish-proxy-service/src/main/java/dev/mayank/ishproxyservice/controller/ContactProxyRestController.java
+++ sb_17-ish-proxy-service/src/main/java/dev/mayank/ishproxyservice/controller/ContactProxyRestController.java
@@ -1,12 +1,16 @@
 package dev.mayank.ishproxyservice.controller;
 
 import dev.mayank.ishproxyservice.model.ContactDetail;
+import dev.mayank.ishproxyservice.model.Response;
 import dev.mayank.ishproxyservice.proxy.ContactMsgProxy;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -15,15 +19,30 @@ import java.util.List;
 @SuppressWarnings("unused")
 public class ContactProxyRestController {
     private final ContactMsgProxy contactMsgProxy;
+    private final RestTemplate restTemplate;
 
     @Autowired
-    public ContactProxyRestController(ContactMsgProxy contactMsgProxy) {
+    public ContactProxyRestController(ContactMsgProxy contactMsgProxy, RestTemplate restTemplate) {
         this.contactMsgProxy = contactMsgProxy;
+        this.restTemplate = restTemplate;
     }
 
     @GetMapping("/getMessages")
     public List<ContactDetail> getMessages(@RequestParam("status") String status) {
         log.info("Fetching messages from contact-msg-service");
         return contactMsgProxy.getMessagesByStatus(status);
+    }
+
+    @PostMapping("/sendMessage")
+    public ResponseEntity<Response> sendMessage(@RequestBody ContactDetail message) {
+        log.info("Sending message to contact-msg-service");
+        String uri = "http://localhost:8080/api/contact/saveMessage";
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Content-Type", "application/json");
+        httpHeaders.add("invocationFrom", "restTemplate");
+        HttpEntity<ContactDetail> httpEntity = new HttpEntity<>(message, httpHeaders);
+        ResponseEntity<Response> responseEntity = restTemplate.exchange(
+                uri, HttpMethod.POST, httpEntity, Response.class);
+        return responseEntity;
     }
 }
